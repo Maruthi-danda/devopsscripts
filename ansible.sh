a5a56336ebc7e183106bb56beb6e25c85841001d
Maruthi-danda <d.maruthi96@gmail.com>
2025-09-09 11:29:40 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,4 @@
-amazon-linux-extras install ansible2 -y
-yum install python3 python-pip python-dlevel -y
+#amazon-linux-extras install ansible2 -y
+#yum install python3 python-pip python-dlevel -y
+sudo dnf install ansible -y
+sudo dnf install python3 python3-pip -y
